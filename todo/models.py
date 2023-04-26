3675dd53dbc08091b62186e49a4b57325813b74c
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-04-26 21:55:38 +0000

    Rendering data from db to browser

--- todo/models.py
+++ todo/models.py
@@ -6,5 +6,6 @@ class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
 
+    # Override the __str__ method to return the name of the item
     def __str__(self):
         return self.name
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from .models import Item
 
 
 # Create your views here.
-def get_todo_list(request):
-    return render(request, 'todo/todo_list.html')
+def get_todo_list(request):   # Query the database to retrieve all items 
+    items = Item.objects.all()
+    context = {
+        'items': items  # Create a dictionary with 'items' key and value
+                        # as items variable
+
+    }
+    return render(request, 'todo/todo_list.html', context) 
+    # Render the template with context dictionary and
+    # return the HttpResponse object
