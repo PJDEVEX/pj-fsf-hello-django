b741b1f4ce6b23754fdfa65f9bf6a98429549d85
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-04-27 22:42:39 +0000

    Adding create functionality to db

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Item
 
 
@@ -18,5 +18,13 @@ def get_todo_list(request):   # Query the database to retrieve all items
 def add_item(request):
     """
     Renders the 'add_item.html' template when the add_item view is requested.
+    If a POST request is made, it creates a new item with the given name
+    and 'done' status and redirects to the 'get_todo_list' view.
     """
-    return render(request, 'todo/add_item.html')
+    if request.method == 'POST':  # Check if request method is POST
+        name = request.POST.get('item_name')  # Get item name from POST data
+        done = 'done' in request.POST   # Check if 'done' checkbox is checked
+        Item.objects.create(name=name, done=done)  # Create new item
+        return redirect('get_todo_list')  # Redirect to 'get_todo_list' view
+    return render(request, 'todo/add_item.html')  
+    # Render 'add_item.html' template for GET requests
