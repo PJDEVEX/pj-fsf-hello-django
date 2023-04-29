530f43d45ef9cb1a228bf949dd649e935185a109
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-04-29 12:20:24 +0000

    Testing forms file

--- todo/tests_forms.py
+++ todo/tests_forms.py
@@ -1,10 +1,21 @@
 from django.test import TestCase
+from .forms import ItemForm
 
 
-class TestDjango (TestCase):
+class TestItemForm (TestCase):
 
-    def test_this_thing_works(self):
+    def test_item_name_is_required(self):
         """
         Ensure that this thing works correctly.
         """
-        self.assertEqual(1, 1)
+        form = ItemForm({'name': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('name', form.errors.keys())
+        self.assertEqual(form.errors['name'][0], 'This field is required.')
+
+    def test_done_field_are_explicit_in_form_mataclass(self):
+        """
+        Ensure that this thing works correctly.
+        """
+        form = ItemForm()
+        self.assertEqual(form.Meta.fields, ['name', 'done'])
