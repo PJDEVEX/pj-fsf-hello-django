8502a6a7a33ad79bda653d4804a9a29672bf1b00
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-04-30 16:29:11 +0000

    Bug fix

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 # Import necessary modules
 from pathlib import Path
 import os
+import dj_database_url
 if os.path.isfile("env.py"):
     import env
 # Get the value of DATABASE_URL environment variable
